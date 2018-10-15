fe6eeb67fb6de3944db8f48e3ebd798a0868903a
SONG Seokwon <softcontext@gmail.com>
2018-10-15 14:13:02 +0900

    first c# applciation

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] a)
+        {
+            Console.WriteLine("Hello C#~" + a[0]);
+            Console.WriteLine("Hello C#~" + a[0]);
+            Console.WriteLine("Hello C#~" + a[0]);
+        }
+    }
+}
