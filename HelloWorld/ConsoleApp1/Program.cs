7a53e07406262503384105ece54241217e078a81
SONG Seokwon <softcontext@gmail.com>
2018-10-15 15:23:42 +0900

    day1 c# example

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class TypeCastExample1
+{
+    public static void Main()
+    {
+        short a = 10;
+        int b = a;
+        int c = 50000; //이진수로 “0000 0000 0000 0000 1100 0011 0101 0000”
+                       //short형으로 변환하면 부호비트가 0에서 1로, 음수가 된다.
+        try
+        {
+            short d = checked((short)c); 
+            Console.WriteLine("Short : {0}", d);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("예외: {0}",e.StackTrace);
+            Console.WriteLine("예외: " + e.StackTrace);
+        }        
+    }
+}
---
+++ HelloWorld/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class MyConstants
+    {
+        public const double PI = 3.14;  //ststic final
+        public const int MYAGE = 22;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 2;
+            double area = MyConstants.PI * (radius * radius);
+ Console.WriteLine("Area = {0}, Age={1}", area, MyConstants.MYAGE);
+            //MyConstants.MYAGE = 20; //오류
+            const string name = "홍길동";
+            Console.WriteLine("name : " + name);
+            //name = "김길동"; //오류
+        }
+    }
+}
