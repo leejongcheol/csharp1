48ac6641ba96ddb9b5fb80381ddb3dab12add75e
Kwon <finalsoul1@gmail.com>
2018-10-22 14:01:23 +0900

    day5

---
+++ DeligateTest/DeligateTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DeligateTest
+{
+    class Delegate1
+    {       
+        static void Main(string[] args)
+        {
+            Delegate1 s = new Delegate1();
+            Action<int,int> myMethod = s.Sum;
+            myMethod(10, 30);
+        }
+
+        void Sum(int i, int j)
+        {
+            Console.WriteLine(i+j);
+        }
+    }
+}
